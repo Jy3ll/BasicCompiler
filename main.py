27097e4930f6000d7f12ceaef6efd35019ca1b33
Jy3ll <72228079+Jy3ll@users.noreply.github.com>
2022-01-25 23:37:59 +0100

    Added user input

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from BasicCompiler import Compiler
+import os
 
 if __name__ =='__main__':
-    my_compiler= Compiler()
-    my_compiler.compile('testing_conversion.b')
+    filename=input("Please enter the name of file you want to compile: ")
+    if not os.path.isfile(filename):
+        print("Such file doesn't exist")
+    else:
+        my_compiler= Compiler()
+        my_compiler.compile(filename)
